925f4e50748629ea5ec2b5ca6f244595cf1774f3
howan <oke2103@gmail.com>
2022-10-11 21:53:18 +0900

    초기 프로젝트 설정

---
+++ src/test/java/com/umo/UmoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UmoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
